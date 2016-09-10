feab39cb5d6d6bd206affede433631721b0f1f58
Chao Chen <eric@monkii.com>
2016-09-10 20:27:56 +1000

    main: use creepManger.manageCreeps() method.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var gameSettings = require('game.settings');
+
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
@@ -6,42 +8,44 @@ var creepManager = require('creep.manager');
 
 module.exports.loop = function () {
 
-  // var tower = Game.getObjectById('165d3aed5660381a7fd71d7b');
-  // if (tower) {
-  //     var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-  //         filter: (structure) => structure.hits < structure.hitsMax
-  //     });
-  //     if(closestDamagedStructure) {
-  //         tower.repair(closestDamagedStructure);
-  //     }
+    // var tower = Game.getObjectById('165d3aed5660381a7fd71d7b');
+    // if (tower) {
+    //     var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+    //         filter: (structure) => structure.hits < structure.hitsMax
+    //     });
+    //     if(closestDamagedStructure) {
+    //         tower.repair(closestDamagedStructure);
+    //     }
 
-  //     var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-  //     if (closestHostile) {
-  //         tower.attack(closestHostile)
-  //     }
-  // }
+    //     var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    //     if (closestHostile) {
+    //         tower.attack(closestHostile)
+    //     }
+    // }
 
-  for(var name in Memory.creeps) {
-    if(!Game.creeps[name]) {
-      delete Memory.creeps[name];
-      console.log('Clearing non-existing creep memory:', name);
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
     }
-  }
 
-  creepManager.keep(Game.creeps, 'harvester', 2);
-  creepManager.keep(Game.creeps, 'upgrader', 2);
-  creepManager.keep(Game.creeps, 'builder', 2);
+    creepManager.manageCreeps(gameSettings);
 
-  for(var name in Game.creeps) {
-    var creep = Game.creeps[name];
-    if(creep.memory.role == 'harvester') {
-      roleHarvester.run(creep);
+    // creepManager.changeRole(Game.creeps, 'harvester', 'upgrader', 2);
+    
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+
+        // creep.moveTo(11, 15);
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
     }
-    if(creep.memory.role == 'upgrader') {
-      roleUpgrader.run(creep);
-    }
-    if(creep.memory.role == 'builder') {
-      roleBuilder.run(creep);
-    }
-  }
 }
