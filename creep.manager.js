72e05274641254427610528097b444081331305f
Chao Chen <eric@monkii.com>
2016-09-10 19:08:25 +1000

    Creep manager: use promise.

--- creep.manager.js
+++ creep.manager.js
@@ -1,32 +1,54 @@
 var creepManager = {
-    keep: function (creeps, role, max) {
-      var targetCreeps = this.filterRole(creeps, role);
-      // console.log('Harvesters: ' + harvesters.length);
+  keep: function (creeps, role, max) {
+    var targetCreeps = this.filterRole(creeps, role);
 
-      if(targetCreeps.length < max) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: role});
-      }
-    },
+    if(targetCreeps.length < max) {
+      console.log(`There is ${targetCreeps.length} ${role} creeps.`);
 
-    changeRole: function (creeps, oldRole, newRole, number) {
-      var targetCreeps = this.filterRole(creeps, oldRole);
+      const tryToCreatCreep = (spawn, role, body, name) => new Promise((resolve, reject) => {
+        const result = spawn.canCreateCreep(body, name, {role: role});
 
-      var counter = 0;
-      for (var name in targetCreeps) {
-        if (counter >= number) {
-          break;
+        const createCreep = (spawn, role, body = [WORK,CARRY,MOVE], name) => {
+          const newName = spawn.createCreep(body, name, {role: role});
+          console.log(`New ${role} creep is created, name: ${newName}.`);
         }
-        this.swapRole(targetCreeps[name], newRole);
-        counter++;
-      }
-    },
 
-    filterRole: (creeps, role) => _.filter(creeps, (creep) => {creep.memory.role == role}),
+        const cannotCreate = (error) => console.log(`Cannot create creep, error ${error}`);
 
-    swapRole: (creep, newRole) => {
-      creep.memory.oldRole = creep.memory.role;
-      creep.memory.role = newRole;
+        if (result == OK) {
+          createCreep(spawn, role, body, name);
+          resolve();
+        }
+        else {
+          cannotCreate(result);
+          reject();
+        }
+      });
+
+      // @todo sort out whey promise does not work.
+      tryToCreatCreep(Game.spawns['Spawn1'], role, [WORK,CARRY,MOVE]).then(()=>console.log('resolve')).catch(()=>console.log('reject'));
     }
+  },
+
+  changeRole: function (creeps, oldRole, newRole, number) {
+    var targetCreeps = this.filterRole(creeps, oldRole);
+
+    var counter = 0;
+    for (var name in targetCreeps) {
+      if (counter >= number) {
+        break;
+      }
+      this.swapRole(targetCreeps[name], newRole);
+      counter++;
+    }
+  },
+
+  filterRole: (creeps, role) => _.filter(creeps, creep => creep.memory.role == role),
+
+  swapRole: (creep, newRole) => {
+    creep.memory.oldRole = creep.memory.role;
+    creep.memory.role = newRole;
+  }
 
 }
 
