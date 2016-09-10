f0a903a761b19fe31aca149c644c5385657ab381
Chao Chen <eric@monkii.com>
2016-09-10 20:27:10 +1000

    creep.manager: - apply 4 spaces indent - add method manageCreeps - use game.settings

--- creep.manager.js
+++ creep.manager.js
@@ -1,54 +1,60 @@
 var creepManager = {
-  keep: function (creeps, role, max) {
-    var targetCreeps = this.filterRole(creeps, role);
-
-    if(targetCreeps.length < max) {
-      console.log(`There is ${targetCreeps.length} ${role} creeps.`);
-
-      const tryToCreatCreep = (spawn, role, body, name) => new Promise((resolve, reject) => {
-        const result = spawn.canCreateCreep(body, name, {role: role});
-
-        const createCreep = (spawn, role, body = [WORK,CARRY,MOVE], name) => {
-          const newName = spawn.createCreep(body, name, {role: role});
-          console.log(`New ${role} creep is created, name: ${newName}.`);
+    manageCreeps: function (settings)  {
+        for (let role in settings.creeps)  {
+            this.keep(Game.creeps, role, settings.creeps[role]);
         }
+    },
 
-        const cannotCreate = (error) => console.log(`Cannot create creep, error ${error}`);
+    keep: function (creeps, role, max) {
+        var targetCreeps = this.filterRole(creeps, role);
 
-        if (result == OK) {
-          createCreep(spawn, role, body, name);
-          resolve();
+        if(targetCreeps.length < max) {
+            console.log(`There is ${targetCreeps.length} ${role} creeps.`);
+
+            const tryToCreatCreep = (spawn, role, body, name) => new Promise((resolve, reject) => {
+                const result = spawn.canCreateCreep(body, name, {role: role});
+
+                const createCreep = (spawn, role, body = [WORK,CARRY,MOVE], name) => {
+                    const newName = spawn.createCreep(body, name, {role: role});
+                    console.log(`New ${role} creep is created, name: ${newName}.`);
+                }
+
+                const cannotCreate = (error) => console.log(`Cannot create creep, error ${error}`);
+
+                if (result == OK) {
+                    createCreep(spawn, role, body, name);
+                    resolve();
+                }
+                else {
+                    cannotCreate(result);
+                    reject();
+                }
+            });
+
+            // @todo sort out whey promise does not work.
+            tryToCreatCreep(Game.spawns['Spawn1'], role, [WORK,CARRY,MOVE]).then(()=>console.log('resolve')).catch(()=>console.log('reject'));
         }
-        else {
-          cannotCreate(result);
-          reject();
-        }
-      });
+    },
 
-      // @todo sort out whey promise does not work.
-      tryToCreatCreep(Game.spawns['Spawn1'], role, [WORK,CARRY,MOVE]).then(()=>console.log('resolve')).catch(()=>console.log('reject'));
+    changeRole: function (creeps, oldRole, newRole, number) {
+        var targetCreeps = this.filterRole(creeps, oldRole);
+
+        var counter = 0;
+        for (var name in targetCreeps) {
+            if (counter >= number) {
+                break;
+            }
+            this.swapRole(targetCreeps[name], newRole);
+            counter++;
+        }
+    },
+
+    filterRole: (creeps, role) => _.filter(creeps, creep => creep.memory.role == role),
+
+    swapRole: (creep, newRole) => {
+        creep.memory.oldRole = creep.memory.role;
+        creep.memory.role = newRole;
     }
-  },
-
-  changeRole: function (creeps, oldRole, newRole, number) {
-    var targetCreeps = this.filterRole(creeps, oldRole);
-
-    var counter = 0;
-    for (var name in targetCreeps) {
-      if (counter >= number) {
-        break;
-      }
-      this.swapRole(targetCreeps[name], newRole);
-      counter++;
-    }
-  },
-
-  filterRole: (creeps, role) => _.filter(creeps, creep => creep.memory.role == role),
-
-  swapRole: (creep, newRole) => {
-    creep.memory.oldRole = creep.memory.role;
-    creep.memory.role = newRole;
-  }
 
 }
 
---
+++ game.settings.js
@@ -0,0 +1,9 @@
+var gameSettings = {
+    creeps: {
+        harvester: 3,
+        upgrader: 2,
+        builder: 1
+    }
+}
+
+module.exports = gameSettings;
