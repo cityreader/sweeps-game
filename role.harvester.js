e24285c23b62561080717c889395fc08f4f159ef
Chao Chen <eric@monkii.com>
2016-09-11 15:54:31 +1000

    role.harvester: use 4 spaces indent.

--- role.harvester.js
+++ role.harvester.js
@@ -1,29 +1,33 @@
 var roleHarvester = {
 
-  /** @param {Creep} creep **/
-  run: function(creep) {
-    if(creep.carry.energy < creep.carryCapacity) {
-      var sources = creep.room.find(FIND_SOURCES);
-      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
-      }
-    }
-    else {
-      var targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-            return (structure.structureType == STRUCTURE_EXTENSION ||
-          structure.structureType == STRUCTURE_SPAWN ||
-          structure.structureType == STRUCTURE_TOWER) &&
-        structure.energy < structure.energyCapacity;
-    }
-    });
-      if(targets.length > 0) {
-        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0]);
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        creep.say('havester');
+
+        if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1]);
+            }
         }
-      }
+        else {
+            // Move to Flag2 to not occupy mining place.
+            creep.moveTo(Game.flags.Flag2);
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) &&
+                        structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+        }
     }
-  }
 };
 
 module.exports = roleHarvester;
