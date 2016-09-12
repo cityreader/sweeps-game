99548fec4f729ef22302b96a82bbb938c903141a
Eric Chen <eric.chenchao@gmail.com>
2016-09-12 20:53:39 +1000

    Add Grunt js to auto deploy code.

---
+++ Gruntfile.example.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-screeps');
+
+    grunt.initConfig({
+        copy: {
+            files: {
+                cwd: 'app',  // set working folder / root to copy
+                src: '**/*',           // copy all files and subfolders
+                dest: 'dist',    // destination folder
+                expand: true           // required when using cwd
+            }
+        },
+
+        screeps: {
+            options: {
+                email: '<your e-mail>',
+                password: '<your password>',
+                branch: 'default',
+                ptr: false
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        }
+    });
+
+    // Default task(s).
+    grunt.registerTask('default', ['copy']);
+    grunt.registerTask('deploy', ['copy', 'screeps']);
+}
