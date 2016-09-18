b2fbe928fecb4f977edac4d5abdcaa05222b3b6a
Chao Chen <eric.chenchao@gmail.com>
2016-09-18 22:40:40 +1000

    Sort container before transferring.

--- app/role.mover.js
+++ app/role.mover.js
@@ -38,6 +38,9 @@ const roleMover = {
 
             // Fetch energy from container.
             if (containers.length > 0) {
+                containers = containers.filter(container => container.store);
+                containers.sort((a, b) => b.store[RESOURCE_ENERGY] - a.store[RESOURCE_ENERGY])
+
                 if (containers[0].transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(containers[0]);
                 }
