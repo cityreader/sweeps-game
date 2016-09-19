937b46a64718526530dcf1a87f147bbd7b543db4
Chao Chen <eric.chenchao@gmail.com>
2016-09-19 22:15:45 +1000

    Enable tower behaviour.

--- app/main.js
+++ app/main.js
@@ -53,20 +53,20 @@ module.exports.loop = function () {
 
     ai.run();
 
-    // var tower = Game.getObjectById('165d3aed5660381a7fd71d7b');
-    // if (tower) {
-    //     var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-    //         filter: (structure) => structure.hits < structure.hitsMax
-    //     });
-    //     if(closestDamagedStructure) {
-    //         tower.repair(closestDamagedStructure);
-    //     }
+    var tower = Game.getObjectById('57dfbbc6adafdf710cbecdff');
+    if (tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
 
-    //     var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    //     if (closestHostile) {
-    //         tower.attack(closestHostile)
-    //     }
-    // }
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
+            tower.attack(closestHostile)
+        }
+    }
 
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
